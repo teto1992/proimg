dc592543af04b9caffff3351959249c225135b8d
Stefano <stefano.forti92@gmail.com>
2023-07-21 18:19:19 +0200

    iterative deepening

--- lp_main.pl
+++ lp_main.pl
@@ -1,33 +1,44 @@
 :-consult('infra.pl').
 :-consult('images.pl').
 
+maxReplicas(4).
+
 % placement/2 returns an image Placement and its Cost.
 placement(Placement, Cost) :-
-    findall(I, image(I,_,_), Images), 
-    imagePlacement(Images, Placement, Cost).
+    findall(I, image(I,_,_), Images), maxReplicas(Max), imagePlacement(Images, Placement, Cost, Max).
 
-% bestPlacement/2 returns a cost-optimal image Placement and its Cost.
-bestPlacement(Placement, Cost) :-
-    findall(I, image(I,_,_), Images), 
-    imagePlacement(Images, Placement, Cost),
-    \+ ( imagePlacement(Images, P2, C2), dif(Placement, P2), C2 < Cost ).
+iterativeDeepening(Placement, Cost) :-
+    maxReplicas(Max), iterativeDeepening(Placement, Cost, 1, Max), !.
 
-% imagePlacement/3 recursively places images on nodes by using replicaPlacement/5.
-imagePlacement([I|Is], Placement, Cost) :-
-    imagePlacement(Is,P,C), 
-    replicaPlacement(I,P,Placement,C,Cost).
-imagePlacement([],[],0).
+iterativeDeepening(Placement, Cost, M, Max) :-
+    M =< Max, \+ bestPlacement(Placement, Cost, M),
+    write(M), nl, NewM is M+1,
+    iterativeDeepening(Placement, Cost, NewM, Max).
+iterativeDeepening(Placement, Cost, M, Max) :-
+    M =< Max, bestPlacement(Placement, Cost, M).
+
+% bestPlacement/2 returns a cost-optimal image Placement and its Cost,
+% with at most Max replicas per image.
+bestPlacement(Placement, Cost, Max) :-
+    findall(I, image(I,_,_), Images), 
+    imagePlacement(Images, Placement, Cost, Max), 
+    \+ ( imagePlacement(Images, P2, C2, Max), dif(Placement, P2), C2 < Cost ).
+
+% imagePlacement/3 recursively places (at most Max replica) images on nodes by using replicaPlacement/5.
+imagePlacement([I|Is], Placement, Cost, Max) :-
+    imagePlacement(Is,P,C,Max), replicaPlacement(I,P,Placement,C,Cost,Max).
+imagePlacement([],[],0,_).
 
 % replicaPlacement/5 repeatedly places an image I onto a set of nodes, 
 % by extending Placement into NewPlacement until transferTimesOk/2 holds.
 % It computes the NewCost of NewPlacement by updating the OldCost of Placement.
-replicaPlacement(I, Placement, NewPlacement, OldCost, NewCost) :-
-    \+ transferTimesOk(I, Placement),
+replicaPlacement(I, Placement, NewPlacement, OldCost, NewCost, M) :-
+    \+ transferTimesOk(I, Placement), M>0, NewM is M-1,
     image(I, Size, _), node(N, Storage, C), \+ member(at(I,N), Placement),
     usedHw(Placement, N, UsedHw), Storage - UsedHw >= Size,
     TmpCost is C * Size + OldCost,
-    replicaPlacement(I,[at(I, N)|Placement],NewPlacement,TmpCost, NewCost).
-replicaPlacement(I, P, P, C, C) :- transferTimesOk(I, P).
+    replicaPlacement(I,[at(I, N)|Placement],NewPlacement,TmpCost, NewCost,NewM).
+replicaPlacement(I, P, P, C, C,_) :- transferTimesOk(I, P).
 
 % transferTimesOk/2 checks whether the transfer times of an image I towards all Nodes
 % are met by placement P.
